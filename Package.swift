828b9b1ccd7374fe087a0b24fcb565a3b8e9e742
sjalkote <75491816+sjalkote@users.noreply.github.com>
2025-07-24 07:39:06 -0500

    Bluesocket dependency renamed

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     .library(name: "SwordRPC", targets: ["SwordRPC"])
   ],
   dependencies: [
-    .package(url: "https://github.com/IBM-Swift/BlueSocket.git", from: "2.0.4")
+    .package(url: "https://github.com/Kitura/BlueSocket.git", from: "2.0.2")
   ],
   targets: [
     .target(
