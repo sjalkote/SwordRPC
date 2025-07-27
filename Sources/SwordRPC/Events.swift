675db65ce9227ed62de15333cbd57aa6454132bc
sjalkote <75491816+sjalkote@users.noreply.github.com>
2025-07-26 22:00:50 -0500

    documentation for `Events.swift`

--- Sources/SwordRPC/Events.swift
+++ Sources/SwordRPC/Events.swift
@@ -6,42 +6,44 @@
 //  Copyright © 2017 Alejandro Alonso. All rights reserved.
 //
 
+/// Provides event handler registration methods for SwordRPC.
+///
+/// Use these methods to set closures that respond to RPC events
+/// like connection, disconnection, errors, joining, and spectating.
 extension SwordRPC {
-
-    public func onConnect(
-        handler: @escaping (_ rpc: SwordRPC) -> ()
-    ) {
+    /// Registers a handler to be called when the RPC client connects to Discord.
+    /// - Parameter handler: Closure called with the SwordRPC instance.
+    public func onConnect(handler: @escaping (_ rpc: SwordRPC) -> ()) {
         self.connectHandler = handler
     }
 
-    public func onDisconnect(
-        handler: @escaping (_ rpc: SwordRPC, _ code: Int?, _ msg: String?) -> ()
-    ) {
+    /// Registers a handler to be called when the RPC client disconnects from Discord.
+    /// - Parameter handler: Closure called with the SwordRPC instance, disconnection code, and message.
+    public func onDisconnect(handler: @escaping (_ rpc: SwordRPC, _ code: Int?, _ msg: String?) -> ()) {
         self.disconnectHandler = handler
     }
 
-    public func onError(
-        handler: @escaping (_ rpc: SwordRPC, _ code: Int, _ msg: String) -> ()
-    ) {
+    /// Registers a handler to be called when the RPC client receives an error from Discord.
+    /// - Parameter handler: Closure called with the SwordRPC instance, error code, and message.
+    public func onError(handler: @escaping (_ rpc: SwordRPC, _ code: Int, _ msg: String) -> ()) {
         self.errorHandler = handler
     }
 
-    public func onJoinGame(
-        handler: @escaping (_ rpc: SwordRPC, _ secret: String) -> ()
-    ) {
+    /// Registers a handler to be called when another user joins the game/activity.
+    /// - Parameter handler: Closure called with the SwordRPC instance and join secret.
+    public func onJoinGame(handler: @escaping (_ rpc: SwordRPC, _ secret: String) -> ()) {
         self.joinGameHandler = handler
     }
 
-    public func onSpectateGame(
-        handler: @escaping (_ rpc: SwordRPC, _ secret: String) -> ()
-    ) {
+    /// Registers a handler to be called when a spectate game request is received from another user.
+    /// - Parameter handler: Closure called with the SwordRPC instance and spectate secret.
+    public func onSpectateGame(handler: @escaping (_ rpc: SwordRPC, _ secret: String) -> ()) {
         self.spectateGameHandler = handler
     }
 
-    public func onJoinRequest(
-        handler: @escaping (_ rpc: SwordRPC, _ request: JoinRequest, _ secret: String) -> ()
-    ) {
+    /// Registers a handler to be called when a join request is received from another user.
+    /// - Parameter handler: Closure called with the SwordRPC instance, join request, and join secret.
+    public func onJoinRequest(handler: @escaping (_ rpc: SwordRPC, _ request: JoinRequest, _ secret: String) -> ()) {
         self.joinRequestHandler = handler
     }
-
 }
